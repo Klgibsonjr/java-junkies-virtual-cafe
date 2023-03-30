79aeccec5ad4e944a62b9628571f0041412da8d1
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2023-03-29 21:18:16 -0700

    resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,35 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type 
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedDrinks: [Drink]
+    }   
+
+    type Drink {
+        drinkId: ID!
+        name: String!
+        description: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+
+    type Query {
+        user: [User] 
+        drink(name: )
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+
 `
+
+module.exports = typeDefs;
