4743d7745b1c6962b8ec7273dae1513c94105810
sieraford <114167010+sieraford@users.noreply.github.com>
2023-04-03 20:31:09 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/searched', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
 })
 
